e497726bad4ba4b7bdb9d26d7ad088b2c680668b
KishenSharma6 <ksharma6@proton.me>
2022-11-11 06:48:35 -0800

    Minor changes to syntax

--- src/datasets.py
+++ src/datasets.py
@@ -1,10 +1,12 @@
+from lib2to3.pgen2.token import tok_name
 import torch
 
 
 class Dataset:
-    def __init__(self, data, targets) -> None:
+    def __init__(self, data, targets, tokenizer) -> None:
         self.data = data
         self.targets = targets
+        self.tokenizer = tokenizer
 
     def __len__(self):
         return len(self.data)
@@ -12,3 +14,13 @@ class Dataset:
     def __getitem__(self, idx):
         text = self.data[idx]
         label = self.targets[idx]
+
+        #input_ids are tokens
+        input_ids = self.tokenizer(text)
+
+        #Padding
+        
+        return {
+            "text": torch.tensor(input_ids, dtype=torch.long),
+            "labels": torch.tensor(label)
+        }
