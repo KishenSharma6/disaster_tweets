fde18ea60d6cd108e5d6c02ef5ac2388d488e7dc
KishenSharma6 <ksharma6@proton.me>
2022-11-15 06:42:27 -0800

    First commit

---
+++ src/evaluation.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import sklearn
+
+
+class Metrics:
+    def __init__(self, y_hat, y) -> None:
+        self.preds = y_hat
+        self.labels = y
+    
+    def confusion_matrix(self):
+    
+        pass
+
+
+    
+    def pre_rec_curve(self):
+    
+        pass
+
+
+    
+    def aoc_curve(self):
+    
+        pass
